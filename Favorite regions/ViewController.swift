e4b7c26803953457260b5953b1a915120623764b
Solnyshkoo <kepetrova@edu.hse.ru>
2023-09-23 17:18:31 +0300

    Initial Commit

---
+++ Favorite regions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Favorite regions
+//
+//  Created by Ksenia Petrova on 23.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
